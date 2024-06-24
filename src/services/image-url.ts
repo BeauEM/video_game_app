f3de61c3ef80776e8697b410e00900655b86957d
BeauEM <beau.d.elliott1@gmail.com>
2024-06-24 13:52:47 -0600

    added crop to returned img sizes

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+//insert crop into img url returned from api to reduce file size
+
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImageUrl;
