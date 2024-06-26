9207d162d7284da82c2894d7feafb92ec43e6897
BeauEM <beau.d.elliott1@gmail.com>
2024-06-26 12:03:35 -0600

    Handle games without image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,10 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 //insert crop into img url returned from api to reduce file size
+//add default img for games with no img
 
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
