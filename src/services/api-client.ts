acebd3fce8810b3c3f5425e82f5ad3de7efdaf81
BeauEM <beau.d.elliott1@gmail.com>
2024-07-25 15:56:57 -0600

    updating storage for api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const RAWG_KEY = import.meta.env.VITE_RAWG_API_KEY;
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_RAWG_API_KEY,
+    key: RAWG_KEY,
   },
 });
